c571dcdb53b80e859bcded11a98e57a58c321f0e
ocastrejon <ocastrejon@ucol.mx>
2017-04-23 00:46:27 -0500

    nuevo inventario con listas

---
+++ Nuevo Inventario con Listas/Nuevo Inventario con Listas/Inventario.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nuevo_Inventario_con_Listas
+{
+    class Inventario
+    {
+        public Producto inicio;
+        Producto temp;
+
+        public void Agregar(Producto nuevo)
+        {
+            if (inicio == null)
+                inicio = nuevo;
+            else
+            {
+                temp = inicio;
+                while (temp.siguiente != null)
+                    temp = temp.siguiente;
+                temp.siguiente = nuevo;
+            }
+        }
+
+        public Producto Buscar(int cod)
+        {
+            Producto x = null;
+            Producto b = inicio;
+            while (b != null)
+            {
+                if (b.codigo == cod)
+                {
+                    x = b;
+                    break;
+                }
+                else
+                    b = b.siguiente;
+            }
+            return x;
+        }
+
+        public void Borrar(int cod)
+        {
+            while (inicio != null)
+            {
+                if (inicio.codigo == cod)
+                {
+                    inicio = inicio.siguiente;
+                    break;
+                }
+
+                Producto b = inicio;
+                if (b.siguiente.codigo == cod)
+                {
+                    b.siguiente = b.siguiente.siguiente;
+                    break;
+                }
+                else
+                    b = b.siguiente;
+            }
+        }
+
+        public void Insertar(Producto pr, int a)
+        {
+
+        }
+
+        public string Reporte()
+        {
+            string datos = "";
+            Producto t = inicio;
+            while (t != null)
+            {
+                datos += t.ToString();
+                t = t.siguiente;
+            }
+            return datos;
+        }
+    }
+}
